ef553f9a057bdebf8f0090134ba8411f8fa4deb4
root <root@AY1405311322162025b2Z.(none)>
2014-06-01 17:11:25 +0800

    django 1.8

--- codemirror/urls.py
+++ codemirror/urls.py
@@ -1,9 +1,10 @@
 #-*- coding:utf-8 -*-
-from django.conf.urls.defaults import patterns, url
+from django.conf.urls import patterns, url
 
 urlpatterns = patterns('codemirror.views',
     url(r'^dirlist.html$', 'dirlist'),
     url(r'^index.html$', 'index'),
+    url(r'^$', 'index'),
     url(r'^openfile.html$', 'openfile'),
     url(r'^savefile.html$', 'savefile'),
     url(r'^createfile.html$', 'createfile'),
--- coding/urls.py
+++ coding/urls.py
@@ -2,19 +2,20 @@ from django.conf.urls import patterns, include, url
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
-admin.autodiscover()
+#admin.autodiscover()
 
 urlpatterns = patterns('',
+    url(r'^', include('codemirror.urls')),
     url(r'^codemirror/', include('codemirror.urls')),
     # Examples:
-     url(r'^$', 'coding.views.home', name='home'),
+    #url(r'^$', 'coding.views.home', name='home'),
     # url(r'^coding/', include('coding.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-     url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
 )
 
 
